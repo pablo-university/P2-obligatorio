d5fb7afbe1352ac692742a09c9d8e7e61f4723f8
pablo <pablodelfantexp@gmail.com>
2021-04-26 17:18:11 -0300

    get all products in list products

--- dashboard/list_update_delete.php
+++ dashboard/list_update_delete.php
@@ -1,45 +1,62 @@
 <?php include_once __DIR__ . '/components/layout_dashboard.php'; ?>
-<?php include_once __DIR__.'/components/header_content.php';?>
+<?php include_once __DIR__ . '/components/header_content.php'; ?>
+
 
 
 
 <?php $content_dashboard = function (): void { ?>
+   <?php include_once __DIR__ . '/controllers/get_all_products.php'; ?>
 
-   
-   <?php header_content('Listado, actualización y eliminación de contenido');?>
+
+   <?php header_content('Listado, actualización y eliminación de contenido'); ?>
 
    <table class="table table-striped table-hover">
+
+      <!-- OBTENGO PRODUCTOS -->
+      <?php $res_all_products = $get_all_products(); ?>
+
+
       <thead>
          <tr>
-            <th>#</th>
-            <th First</th>
-            <th>Last</th>
-            <th>Handle</th>
+            <th>Titulo</th>
+            <th>Marca</th>
+            <th>case</th>
+            <th>display</th>
+            <th>momento</th>
+            <th>precio</th>
+            <th>stock</th>
+            <th>Tipo de usuario</th>
+            <th>Peso</th>
          </tr>
       </thead>
       <tbody>
-         <tr>
-            <th>1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-         </tr>
-         <tr>
-            <th>2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-         </tr>
-         <tr>
-            <th>3</th>
-            <td>Larry the Bird</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-         </tr>
+
+
+
+         <!-- PINTO PRODUCTOS -->
+         <?php while ($data = $res_all_products->fetch_object()) { ?>
+
+            <tr>
+               <!-- estos datos estarían incompletos solo serian para mostraer e identificar el producto -->
+               <th><?= $data->title ?></th>
+               <!-- poner la banda aqui, quiza recicle la funcion de producto especifico -->
+               <td><?= $data->brand ?></td>
+               <td><?= $data->case ?></td>
+               <td><?= $data->display_type ?></td>
+               <td><?= $data->moment ?></td>
+               <td><?= $data->price ?></td>
+               <td><?= $data->stock ?></td>
+               <td><?= $data->user_type ?></td>
+               <td><?= $data->weight ?></td>
+            </tr>
+
+         <?php } ?>
+
+
+
       </tbody>
    </table>
 
 <?php } ?>
 
 <?php layout_dashboard($content_dashboard) ?>
-

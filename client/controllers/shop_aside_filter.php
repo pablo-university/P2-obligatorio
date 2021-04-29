2beeb4279cff1205d1583496a51d0eb00a97cffc
pablo <pablodelfantexp@gmail.com>
2021-04-29 10:07:57 -0300

    cree la funcionalidad OR Y AND y estoy por implementarla

--- client/controllers/shop_aside_filter.php
+++ client/controllers/shop_aside_filter.php
@@ -4,6 +4,8 @@
 function shop_aside_filter($card_product, $conn): void
 {
     $query = [];
+    $query_main = [];
+    $sub_query = [];
     /*
     recorro el request, que contiene arrays asociativos de names
     y por cada name recorro para obtener valores (name = value)
@@ -11,11 +13,22 @@ function shop_aside_filter($card_product, $conn): void
     foreach ($_REQUEST as $name => $arr) {
         foreach ($arr as $value) {
             array_push($query, "$name = '$value'");
+            // 1) agregar a sub query
+            array_push($sub_query, "$name = '$value'");
         }
+        // 2) impode con OR
+        $sub_query = implode(" OR ", $sub_query);
+
+        // 3) agrego eso al query principal + paréntesis curvos
+        array_push($query_main, '('.$sub_query.')');
+
+        // 4) vacio sub query
+        $sub_query = [];
     }
     // por ultimo tomo el array query, uso implode y agrego el AND
-    $query = implode(" OR ", $query);//cambie and por or -> imposible color = negro AND color = gris
+    $query = implode(" OR ", $query);//trae/filtra lo que puede, no es estricto
 
+var_dump(implode(" AND ", $query_main));
 
     // COMPLETO MI QUERY
     $query = "
@@ -27,7 +40,7 @@ function shop_aside_filter($card_product, $conn): void
     GROUP BY (P._id)
     ;
     ";
-
+echo "$query";
     $res = mysqli_query($conn, $query);
 
 

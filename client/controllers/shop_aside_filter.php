b8710b3c6457c8a2f5edf6af928762f8f79d32d3
pablo <pablodelfantexp@gmail.com>
2021-04-29 10:46:36 -0300

    filter new caracteristic

--- client/controllers/shop_aside_filter.php
+++ client/controllers/shop_aside_filter.php
@@ -1,46 +1,48 @@
 <?php
 
+/* 
+---- RESUMEN -----
+este filtro: traeme conincidencia con una propiedad con un valor OR varios valores...
+AND se exaustivo en que si seleccionas mas de una propiedad cumpla eso (tener mas de una propiedad en la query) */
+
 // OBTIENE Y CREA EL QUERY
 function shop_aside_filter($card_product, $conn): void
 {
     $query = [];
-    $query_main = [];
     $sub_query = [];
     /*
-    recorro el request, que contiene arrays asociativos de names
+    0) recorro el request, que contiene arrays asociativos de names
     y por cada name recorro para obtener valores (name = value)
-    y dejarlos en un array */
+    e irlos procesando (marque con pasos) */
     foreach ($_REQUEST as $name => $arr) {
         foreach ($arr as $value) {
-            array_push($query, "$name = '$value'");
             // 1) agregar a sub query
             array_push($sub_query, "$name = '$value'");
         }
-        // 2) impode con OR
+        // 2) implode con OR
         $sub_query = implode(" OR ", $sub_query);
 
         // 3) agrego eso al query principal + paréntesis curvos
-        array_push($query_main, '('.$sub_query.')');
+        array_push($query, '(' . $sub_query . ')');
 
         // 4) vacio sub query
         $sub_query = [];
     }
-    // por ultimo tomo el array query, uso implode y agrego el AND
-    $query = implode(" OR ", $query);//trae/filtra lo que puede, no es estricto
+    // 5) por ultimo tomo el array query, uso implode y agrego el AND
+    $query = implode(" AND ", $query); //no es estricto en una porpiedad con valores, pero si cuando tiene varias!
 
-var_dump(implode(" AND ", $query_main));
-
+    
     // COMPLETO MI QUERY
     $query = "
     SELECT P._id, I.url, P.title, P.price, P.sale
-    FROM products AS P
-    INNER JOIN images AS I
+    FROM products P
+    INNER JOIN images I
     ON P._id = I.id_product
     WHERE $query
     GROUP BY (P._id)
     ;
     ";
-echo "$query";
+    echo "$query";
     $res = mysqli_query($conn, $query);
 
 
@@ -60,7 +62,3 @@ echo "$query";
         }
     }
 }
-
-
-
-?>

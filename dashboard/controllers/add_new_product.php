5789710c9b092b9f599d93256028b8110db5f2ac
pablo <pablodelfantexp@gmail.com>
2021-05-03 09:50:30 -0300

    add multiple image to storage

--- dashboard/controllers/add_new_product.php
+++ dashboard/controllers/add_new_product.php
@@ -3,48 +3,60 @@
 // https://www.w3schools.com/php/func_misc_uniqid.asp
 // aqui en este php estaría agregando a la base de datos
 
-echo '<pre>' . var_export($_FILES, true) . '</pre>';
 echo '<pre>' . var_export($_FILES['image'], true) . '</pre>';
 
 // ----------------
 
 
-function upload_image(): void
+function upload_image()
 {
     // si la imagen existe
     if (isset($_FILES['image'])) {
 
         $upload_control = true;
-        $msg = '';
+        $msg = 'todo ok';
+
 
         // type control
-        if (!($_FILES['image']['type'] == "image/jpeg")) {
-            $msg = $msg . " Tu archivo tiene que ser jpg.<BR>";
-            $upload_control = false;
+        foreach ($_FILES['image']['type'] as $key => $value) {
+            if (!($value == "image/jpeg")) {
+                $msg = "Tus archivos tiene que ser jpg.<BR>";
+                $upload_control = false;
+            }
         }
 
         // size control
-        if ($_FILES['image']['size'] > 5242880) {
-            $msg = $msg . "El archivo es mayor que 5mb<BR>";
-            $upload_control = false;
+        foreach ($_FILES['image']['size'] as $key => $value) {
+            if (($value > 5242880)) {
+                $msg = $msg . "Un archivo es mayor que 5mb<BR>";
+                $upload_control = false;
+            }
         }
 
         // target path
         $target_path = __DIR__ . '/../../assets/img/products/test/';
         // echo $target_path;
-        $target_path = $target_path . uniqid() . '.jpg';
+        
 
         // check variable de control
         if ($upload_control) {
 
-            if (move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
-                echo "Ha sido subido satisfactoriamente";
-            } else {
-                echo "Error al subir el archivo";
+            foreach ($_FILES['image']['tmp_name'] as $key => $value) {
+
+                $target_path = $target_path . uniqid() . '.jpg';
+                
+            
+                if (!move_uploaded_file($value, $target_path)) {
+                    return "Error al subir el archivo<br>";
+                }
+
             }
+
+            return "Ha sido subido satisfactoriamente<br>";
         } else {
-            echo $msg;
+            return $msg;
         }
     }
 }
-// upload_image();
+
+echo upload_image();

b8a6f054f0789a5efb79d027eedc155efa3bf22a
pablo <pablodelfantexp@gmail.com>
2021-05-04 08:39:52 -0300

    the dashboard save image

--- dashboard/controllers/add_new_product.php
+++ dashboard/controllers/add_new_product.php
@@ -4,116 +4,163 @@ include_once __DIR__ . '/../../connectors/connection.php';
 
 echo '<pre>' . var_export($_REQUEST, true) . '</pre>';
 
-// set product band
-function set_product_band($conn, $valor)
-{
-    $query = "
-    INSERT INTO product_band (id_product, id_band)
-    VALUES ('$valor', '$_REQUEST[band]');
-    ";
 
-    if (!$conn->query($query)) {
-        return ">>error en set_product_band::<br>$query";
-    } else {
-        return ">>set_product_band correcta::<br>$query";
+
+/**
+ * Agrega nuevo producto
+ * 
+ * agrega producto
+ * luego producto relacion con banda
+ * agrega imagen en servidor
+ * agrega la imagen en sql
+ */
+
+
+
+class Add_new_product
+{
+    public $last_id_inserted = null;
+
+    public function __construct(
+        public $conn
+    ) {
     }
-}
 
-function set_image($conn, $_id_product)  {
-   /*  realizar una inserción en la tabla de imágenes
-    con el id de producto correspondiente */
-}
+    // set product band
+    protected function set_product_band()
+    {
+        // si funciona puedo tomar mi variable global de objeto last_id_inserted
+        $id_inserted = $this->conn->insert_id;
 
-// set product
-function set_product($conn)
-{
-    if (!isset($_REQUEST)) {
-        return 'request isn´t setted';
-    } else {
-        $to_insert = [];
-        $insert_value = [];
-
-        // obtener key value
-        foreach ($_REQUEST as $key => $value) {
-            //no proceso esto ya que lo hago en set_product_band
-            if (!str_contains($key, 'band')) {
-                array_push($to_insert, 'products.' . $key);
-                array_push($insert_value, "'$value'");
-            }
-        }
-
-        $to_insert = implode(', ', $to_insert);
-        $insert_value = implode(', ', $insert_value);
-
-        // create query
         $query = "
-        INSERT INTO products ($to_insert)
-        VALUES ($insert_value);
+        INSERT INTO product_band (id_product, id_band)
+        VALUES ('$id_inserted', '$_REQUEST[band]');
         ";
 
-        echo "Query:: $query<br>";
-
-        // inserto datos
-        if (!$conn->query($query)) {//$conn->query($query)
-            return '>>error en la query';
+        if (!$this->conn->query($query)) {
+            return ">>error en set_product_band::<br>$query";
         } else {
-            // obtengo ultimo _id y lo paso a set_product_band
-            echo set_product_band($conn, $conn->insert_id);
-            return '>>query correcta, datos ingresados!';
+            return ">>set_product_band correcta::<br>$query";
         }
     }
-}
-// echo set_product($conn);
 
-// upload image
-function upload_image()
-{
-    // si la imagen existe
-    if (isset($_FILES['image'])) {
-
-        $upload_control = true;
-        $msg = 'todo ok';
-
-
-        // type control
-        foreach ($_FILES['image']['type'] as $key => $value) {
-            if (!($value == "image/jpeg")) {
-                $msg = "Tus archivos tiene que ser jpg.<BR>";
-                $upload_control = false;
-            }
+    public function set_image($url)
+    {
+        /*  realizar una inserción en la tabla de imágenes
+        con el id de producto correspondiente */
+        $query = "
+        INSERT INTO images (url, id_product, alt, title)
+        VALUES ('$url', '$this->last_id_inserted', 'empty', 'empty');
+        ";
+        if (!$this->conn->query($query)) {
+            return ">>error en set_image::<br>$query";
+        } else {
+            return ">>set_image correcta::<br>$query";
         }
+    }
 
-        // size control
-        foreach ($_FILES['image']['size'] as $key => $value) {
-            if (($value > 5242880)) {
-                $msg = $msg . "Un archivo es mayor que 5mb<BR>";
-                $upload_control = false;
-            }
-        }
+    // upload image
+    public function upload_image()
+    {
+        // si la imagen existe
+        if (isset($_FILES['image'])) {
 
-        // target path
-        $target_path = __DIR__ . '/../../assets/img/products/test/';
-        // echo $target_path;
+            $upload_control = true;
+            $msg = 'todo ok';
 
 
-        // check variable de control
-        if ($upload_control) {
-
-            foreach ($_FILES['image']['tmp_name'] as $key => $value) {
-
-                $target_path = $target_path . uniqid() . '.jpg';
-
-
-                if (!move_uploaded_file($value, $target_path)) {
-                    return "Error al subir el archivo<br>";
+            // type control
+            foreach ($_FILES['image']['type'] as $key => $value) {
+                if (!($value == "image/jpeg")) {
+                    $msg = "Tus archivos tiene que ser jpg.<BR>";
+                    $upload_control = false;
                 }
             }
 
-            return "Ha sido subido satisfactoriamente<br>";
+            // size control
+            foreach ($_FILES['image']['size'] as $key => $value) {
+                if (($value > 5242880)) {
+                    $msg = $msg . "Un archivo es mayor que 5mb<BR>";
+                    $upload_control = false;
+                }
+            }
+
+            // target path
+            $target_path = __DIR__ . '/../../assets/img/products/test/';
+            // echo $target_path;
+
+            // check variable de control
+            if ($upload_control) {
+
+                foreach ($_FILES['image']['tmp_name'] as $key => $value) {
+
+                    $name = uniqid() . '.jpg';
+                    $target_path = $target_path . $name;
+
+                    // mover archivo a directorio
+                    if (!move_uploaded_file($value, $target_path)) {
+                        return "Error al subir al guardar archivo<br>";
+                    }
+
+                    // cada que guarde inserto en sql
+                    $this->set_image($name);
+                }
+
+                return ">>upload_image correcta<br>";
+            } else {
+                return $msg;
+            }
+        }
+    }
+
+    // set product
+    public function set_product()
+    {
+        if (!isset($_REQUEST)) {
+            return 'request isn´t setted';
         } else {
-            return $msg;
+            $to_insert = [];
+            $insert_value = [];
+
+            // obtener key value
+            foreach ($_REQUEST as $key => $value) {
+                //no proceso esto ya que lo hago en set_product_band
+                if (!str_contains($key, 'band')) {
+                    array_push($to_insert, 'products.' . $key);
+                    array_push($insert_value, "'$value'");
+                }
+            }
+
+            $to_insert = implode(', ', $to_insert);
+            $insert_value = implode(', ', $insert_value);
+
+            // create query
+            $query = "
+            INSERT INTO products ($to_insert)
+            VALUES ($insert_value);
+            ";
+
+            echo "Query:: $query<br>";
+
+            // inserto datos
+            if (!$this->conn->query($query)) { //$this->conn->query($query)
+                return '>>error en la query';
+            } else {
+                // AL INSERTAR PRODUCTO SE DESPRENDEN LAS DEMAS
+
+                // obtengo ultimo _id y lo paso a set_product_band
+                $this->last_id_inserted = $this->conn->insert_id;
+
+                echo $this->set_product_band();
+                // aqui ejecutar guardado de imagen
+                echo $this->upload_image();
+
+                return '>>query correcta, datos ingresados!';
+            }
         }
     }
 }
 
-// echo upload_image();
+
+$prueba = new Add_new_product($conn);
+echo $prueba->set_product();

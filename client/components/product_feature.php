2f0a6f04df2e1b48ec563fd9248af44fdc10576a
pablo <pablodelfantexp@gmail.com>
2021-05-31 19:00:15 -0300

    add tooltip in product feature, and modify the navbar in dashboard

--- client/components/product_feature.php
+++ client/components/product_feature.php
@@ -58,12 +58,12 @@ endif;
 
     <div class="text-center">
       <h3 class="mb-0">Colores</h3>
-      <p class="lead text-black-50 fs-6">colores disponible</p>
+      <p class="mb-0 text-black-50">disponibles</p>
       <p class="m-0">
         <?php $res_color = $product_color($_id); ?>
         <?php if ($res_color) { ?>
           <?php while ($data_color = $res_color->fetch_object()) { ?>
-            <i class="bi bi-circle-fill fs-3" title="<?= $data_color->color ?>" style="color:<?= $data_color->code ?>;"></i>
+            <i class="bi bi-circle-fill fs-3" data-bs-toggle="tooltip" data-bs-placement="right" title="<?= $data_color->color ?>" style="color:<?= $data_color->code ?>;"></i>
           <?php } ?>
         <?php } else {?>
         <p>color no asignado</p>
@@ -74,7 +74,7 @@ endif;
 
     <div class="text-center">
       <h3 class="m-0 mb-n3">Marca</h3>
-      <img class="w-50" style="filter: contrast(0.5);" src="<?= LOCAL_HOST . "assets/img/brands/" . $data->brand . '.svg' ?>" alt="<?= $data->brand ?>" title="<?= $data->brand ?>">
+      <img class="w-50" style="filter: contrast(0.5);" src="<?= LOCAL_HOST . "assets/img/brands/" . $data->brand . '.svg' ?>" alt="<?= $data->brand ?>" data-bs-toggle="tooltip" data-bs-placement="right" title="<?= $data->brand ?>">
     </div>
 
     <div class="text-center">
@@ -82,8 +82,8 @@ endif;
       <div>
         <?=
         match (true) {
-          $data->case == 'redondo' => "<i style=\"filter: contrast(0.5);\" class=\"bi bi-watch fs-1\" title=" . $data->case . "></i>",
-          $data->case == 'cuadrado' => "<i style=\"filter: contrast(0.5);\" class=\"bi bi-smartwatch fs-1\" title=" . $data->case . "></i>"
+          $data->case == 'redondo' => "<i style=\"filter: contrast(0.5);\" class=\"bi bi-watch fs-1\" data-bs-toggle=\"tooltip\" data-bs-placement=\"right\" title=" . $data->case ."></i>",
+          $data->case == 'cuadrado' => "<i style=\"filter: contrast(0.5);\" class=\"bi bi-smartwatch fs-1\" data-bs-toggle=\"tooltip\" data-bs-placement=\"right\" title=" . $data->case ."></i>"
         }
         ?>
       </div>

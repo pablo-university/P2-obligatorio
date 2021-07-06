4b86052e4e2c7f553ff13739a9d6febbad13ac4b
pablo <pablodelfantexp@gmail.com>
2021-07-06 09:55:26 -0300

    repair layout cards in index.php on dashboard

--- dashboard/components/index_card_top.php
+++ dashboard/components/index_card_top.php
@@ -9,11 +9,11 @@
 
 
 
-<div class="d-flex justify-content-around justify-content-md-start flex-wrap gap-5">
+<div class="d-flex justify-content-around justify-content-md-start flex-wrap flex-xl-nowrap gap-3">
   <?php foreach ($values_for_get as $key => $value) { ?>
     <?php $res = $get_props_instance->count_value_in_true('products', $key)->fetch_object()->res; ?>
     <!-- <div class="col"> -->
-    <div class="card flex-fill border-0 shadow-sm mb-3 p-0 hover-shadow-lg" style="max-width: 18rem;">
+    <div class="card flex-fill w-100 border-0 shadow-sm mb-3 p-0 hover-shadow-lg" style="max-width: 18rem;">
       <div class="card-header"><?= $key ?></div>
       <div class="card-body text-dark d-flex flex-column">
         <p class="card-text fw-light mb-2"> <?= $value ?></p>
@@ -25,7 +25,7 @@
 
     <!-- static card AVG-->
     <?php $res = $get_props_instance->get_avg('products', 'stock')->fetch_object()->res; ?>
-    <div class="card flex-fill border-0 shadow-sm mb-3 p-0 hover-shadow-lg" style="max-width: 18rem;">
+    <div class="card flex-fill w-100 border-0 shadow-sm mb-3 p-0 hover-shadow-lg" style="max-width: 18rem;">
       <div class="card-header"> Stock promedio </div>
       <div class="card-body text-dark d-flex flex-column">
         <p class="card-text fw-light mb-2"> Promedio de stock de cada producto </p>
